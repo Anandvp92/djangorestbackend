d2c8e93ef679d7aae12b1b5d00359358ecc0422a
Anandvp <anandvp92@gmail.com>
2024-01-16 18:10:19 +0530

    second-commit

--- Rest_Backend/usermanagement/models.py
+++ Rest_Backend/usermanagement/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin,BaseUserManager
 # Create your models here.
+
+
+class User(AbstractBaseUser):
+    email=models.EmailField(verbose_name="Email",max_length=30)
+    phonenumber=models.CharField(verbose_name="Phone Number",max_length=10,min_length=10)
--- Rest_Backend/usermanagement/views.py
+++ Rest_Backend/usermanagement/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(["GET"])
+def register_page(request):
+    return Response ({"message":"Hi i am from the backend"})
